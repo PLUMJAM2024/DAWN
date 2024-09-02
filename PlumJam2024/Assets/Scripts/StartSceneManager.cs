8bb74c3be0919082096bd0eaea76e45e94778931
Hyuni02 <jackjsh@naver.com>
2024-09-02 22:37:51 +0900

    TMP, 시작화면 추가

---
+++ PlumJam2024/Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : MonoBehaviour
+{
+    public void btn_Start() {
+        SceneManager.LoadScene("Player");
+    }
+
+    public void btn_Quit() {
+        Application.Quit();
+    }
+}
